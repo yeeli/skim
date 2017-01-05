f1012fa7af4f1234e48f4dc91da507220b2cfd87
yee.li <yeeli@outlook.com>
2017-01-06 05:55:03 +0800

    fixed sprockets error

--- lib/skim/sprockets.rb
+++ lib/skim/sprockets.rb
@@ -1,6 +1,16 @@
 require "sprockets"
 
-Sprockets.register_engine ".skim", Skim::Template
+if Sprockets.respond_to?(:register_engine)
+  args = ['.skim', Skim::Template]
+  if Sprockets::VERSION.start_with? '3'
+    args << {silence_deprecation: true, mime_type: 'text/skim'}
+  end
+
+  Sprockets.register_engine *args
+else
+  Sprockets.register_mime_type 'text/skim', extensions: ['.skim', '.jst.skim']
+  Sprockets.register_transformer 'text/skim', 'application/javascript', Skim::Template
+end
 
 unless defined?(Rails::Engine)
   Sprockets.append_path File.expand_path('../../../vendor/assets/javascripts', __FILE__)
